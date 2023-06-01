0e735ac0c0f7b0585dc49b5720b8949d082dffc0
dvctrl <davidmouzinho.contato@gmail.com>
2023-06-01 20:20:19 -0300

    login page finalizado

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,13 +2,51 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
+    TextInputEditText idLoginInput, idSenhaInput;
+    Button btnLogin, btnSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        idLoginInput = findViewById(R.id.idLoginInput);
+        idSenhaInput = findViewById(R.id.idSenhaInput);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSair = findViewById(R.id.btnSair);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario, senha;
+
+                usuario = idLoginInput.getText().toString();
+                senha = idSenhaInput.getText().toString();
+
+                if(usuario.equals("admin@email.com") && senha.equals("12345")){
+                    startActivity(new Intent(getApplicationContext(),MenuActivity.class));
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou senha inválidos",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
